e53ac1a53ae1358fa4ab30d64a5a8c834df13d37
Ben <ben.ferris1@gmail.com>
2017-03-12 17:26:31 -0400

    Switched view engine to pug

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 passport.use(new LocalStrategy(
     {
