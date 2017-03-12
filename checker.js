46be8a3500d7c716bfcdc603f30888cab3733ebf
Ben <ben.ferris1@gmail.com>
2017-03-12 01:32:46 -0500

    Removed email sending code from checker, using email-sender instead.

--- checker.js
+++ checker.js
@@ -3,11 +3,13 @@ var sendgrid  = require('sendgrid')(credentials.sendgrid.key);
 var request = require('request');
 var cheerio = require('cheerio');
 var Watch = require('./models/watch');
+var emailSender = require('./email-sender');
 var checker = {};
 
-//TODO: consolidate email sending to use email-sender?
-
-
+/*
+given a term number and crn, parses the corresponding catalog page, extracting seat information and passes it
+to a callback in object form
+*/
 checker.getSection = function(term, crn, callback){
 
     var section = {};
@@ -32,6 +34,7 @@ checker.getSection = function(term, crn, callback){
 
 };
 
+//iterates through all active class watches and sends email notifications if there are available seats
 checker.checkWatches = function (callback) {
     Watch.find({active:true}, function (err, watches) {
         if(err)
@@ -41,7 +44,7 @@ checker.checkWatches = function (callback) {
                 var watch = watches[i];
                 checker.getSection(watch.term, watch.crn, function (err, section) {
                     if(section.availableSeats>0){
-                        checker.sendNotificationEmail(section.title, watch.email, watch.term, watch.crn);
+                        emailSender.sendNotificationEmail(section.title, watch.email, watch.term, watch.crn);
                     }
                 })
             }
@@ -52,27 +55,5 @@ checker.checkWatches = function (callback) {
 
 };
 
-checker.sendNotificationEmail = function (courseName, emailAddress, term, crn) {
-    var email = new sendgrid.Email({
-        to:emailAddress,
-        from: 'notifications@puclasswatch.xyz',
-        fromName:'Class Watcher Notifications',
-        subject:'Now Available: ' + courseName,
-        text:'A section that you are watching now has seats available.\n\nSee details at https://selfservice.mypurdue.purdue.edu/prod/bwckschd.p_disp_detail_sched?term_in='+term+'&crn_in='+crn
-    });
-    sendgrid.send(email);
-
-};
-
-
-checker.testMail = function () {
-    var email = new sendgrid.Email({
-        to:'ben.ferris1@gmail.com',
-        from:'ben@moufee.com',
-        subject:'Testing SetInterval',
-        text:'This message is testing the setInterval functionality of Node.js'
-    });
-    sendgrid.send(email);
-};
 
 module.exports = checker;
--- email-sender.js
+++ email-sender.js
@@ -10,22 +10,21 @@ var test = {
 emailSender.sendNotificationEmail = function (courseName, emailAddress, term, crn) {
     var email = new sendgrid.Email({
         to:emailAddress,
-        from: 'notifications@puclass.space',
-        fromname:'Class Watcher Notifications',
+        from: 'notifications@puclasswatch.xyz',
+        fromName:'Class Watcher Notifications',
         subject:'Now Available: ' + courseName,
         text:'A section that you are watching now has seats available.\n\nSee details at https://selfservice.mypurdue.purdue.edu/prod/bwckschd.p_disp_detail_sched?term_in='+term+'&crn_in='+crn
     });
     sendgrid.send(email);
-
 };
 
-emailSender.testMail = function () {
+emailSender.testMail = function (emailAddress) {
     var email = new sendgrid.Email({
-        to:'ben.ferris1@gmail.com',
+        to:emailAddress,
         from:'notification@puclass.space',
         fromname:'Class Watcher Notifications',
         subject:'Testing SetInterval',
-        text:'This message is testing the setInterval functionality of Node.js'
+        text:'This message tests the email sending functionality.'
     });
     test.sendEmail('hello');
     sendgrid.send(email);
