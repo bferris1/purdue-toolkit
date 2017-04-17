81a62c854e09e69e717ff6ffee0fac2223123e54
Ben <ben.ferris1@gmail.com>
2017-04-17 13:51:34 -0400

    correct cookie maxAge, add request-promise

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(session({
     saveUninitialized:false,
     resave:false,
     cookie: {
-        maxAge: 7*60*60*1000  // 1 week in milliseconds
+        maxAge: 7*24*60*60*1000  // 1 week in milliseconds
     }
 
 }));
