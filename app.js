f975ce0920a018077094007a2ceac93438537058
Ben <ben.ferris1@gmail.com>
2017-09-10 16:50:03 -0400

    move clientID and secret to config.json

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ passport.use(new LocalStrategy(
     }
 ));
 passport.use(new GoogleStrategy({
-        clientID: "569291200494-nk99o76eroeh35rk575lnuc1mthh84fd.apps.googleusercontent.com",
-        clientSecret: "WHdcZlCZEFkhSCbViTzjGW56",
+        clientID: config.auth.google.clientID,
+        clientSecret: config.auth.google.clientSecret,
         callbackURL: applicationURL + "/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
@@ -124,10 +124,9 @@ app.use(passport.session());
 //   request.  The first step in Google authentication will involve
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
-app.get('/auth/google',(req, res) => {
-    if(req.user) res.redirect('/');
+app.get('/auth/google',
     passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/userinfo.email']})
-});
+);
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
